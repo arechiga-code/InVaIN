0aae86ec022bfb3bda96e657cc49cd5066addebf
Alan Arechiga <alanra57@gmail.com>
2017-12-04 22:06:34 -0500

    Change __init__.py for package restructuring

--- InVaIN/__init__.py
+++ InVaIN/__init__.py
@@ -1,46 +1,8 @@
-import requests
-import json
-__version___ = ".01";
+# deprecated to keep older scripts who import this from breaking
+from .Simple import Simple
+from .Advanced import Advanced
 
-BASE_URL = 'https://inva.in/quotes'
 
-class InVaIN():
-        
-        def __init__(self, *symbols):
-                self.tickers = list(symbols)
+__version__ = '0.2.1'
+__all__ = ['Simple', 'Advanced', 'Utils']
 
-        #Add Tickers to End of List, Extend Allows for ticker to be a List                
-        def add_ticker(self, ticker):
-                self.tickers.extend(ticker)
-        #If Someone is Sketch About Singular Form
-        def add_tickers(self, tickers):
-                self.add_ticker(tickers)
-
-        def remove_ticker(self, ticker):
-                self.tickers.remove(ticker)
-                
-        #Returns Ticker List
-        def get_tickers(self):
-                return self.tickers;
-
-        def get_price_string(self):
-                #Prepare the Parameters
-                temp = {'t':','.join(self.tickers)}
-                
-                #Pull Stock Price Data
-                response = requests.get(BASE_URL, params = temp)
-
-                #Parse into CSV string
-                data = ','.join(str(s) for s in response.json().values())
-                return data
-        
-        def get_prices(self):
-                #Prepare the Parameters
-                temp = {'t':','.join(self.tickers)}
-                
-                #Pull Stock Price Data
-                response = requests.get(BASE_URL, params = temp)
-
-                #Parse into Dict Object
-                data = response.json()
-                return data
