afa8a83d29b6f46ea142db9d15ef05caaf2be00c
Alan Arechiga <alanra57@gmail.com>
2018-02-05 21:38:57 -0500

    Change how History constructor works. Added methods for time period data

--- invain/History.py
+++ invain/History.py
@@ -8,16 +8,30 @@ class History():
 
     #Start and End are datetime objects with start being the older of the two dates.
     #Frequency has three options for interval of returned dates with daily = d, weekly = wk, and monthly = mo; All options accept any number n in the form of nd, nwk, or nmo with n being the number of days, weeks, or months
-    def __init__(self, ticker, start=None, end=None, frequency='1d'):
+    def __init__(self, ticker, *extras, **kwargs):
+        if kwargs:
+            #Check if start, end, or frequency are provided, and set accordingly
+            start = kwargs['start'] if 'start' in kwargs else None
+            end = kwargs['end'] if 'end' in kwargs else None
+            frequency = kwargs['frequency'] if 'frequency' in kwargs else '1d'
+        else:
+            start = None
+            end = None
+            frequency = '1d'
+
         now = int(time.mktime(datetime.datetime.now().timetuple()))
-        
-        #Check if ticker var is a list or string then set self.ticker to a list of ticker(s)
-        self.ticker = [symbol.upper() for symbol in ticker] if is_sequence(ticker) else [ticker.upper()]
 
         #Check if start exists - if not default start time is 30 days from now, if start exists set start to strat.
         period1 = now - 30*24*60*60 if start is None else int(time.mktime(start.timetuple()))
         period2 =  now if end is None else int(time.mktime(end.timetuple()))
 
+        #Check if ticker var is a list or string then set self.ticker to a list of ticker(s)
+        self.ticker = [symbol.upper() for symbol in ticker] if is_sequence(ticker) else [ticker.upper()]
+
+        #Add extra tickers if object created with parameters in the form of ('TIC','KER','XTRA')
+        if extras: self.add_tickers([symbol.upper() for symbol in extras])
+        
+        #Set period to a list of unix timestamps, with the first item being the later time.
         self.period = [period1, period2]
         
         self.interval = frequency
@@ -40,14 +54,88 @@ class History():
             else: raise ValueError('InVaIN.Advanced() Failed to remove ticker. Ticker not found in list')
         except ValueError as err:
             print(err.args,"\n Non-Fatal Error")
-                            
+            
     def get_ticker(self):
         return self.ticker
+
+    #Returns list of datetimes instead of unix timestamps
+    def get_period(self):
+        return [datetime.datetime.fromtimestamp(timestamp) for timestamp in self.period]
+    
+    def get_startDate(self):
+        return datetime.datetime.fromtimestamp(self.period[0])
+
+    def get_endDate(self):
+        return datetime.datetime.fromtimestamp(self.period[1])
+
+    #Parameter set to *periods to allow user to provide period in an array (['period1', 'period2']) or individual parameters ('period1', 'period2')
+    def change_period(self, *periods):
+        #if more than two or no parameters provided there is a problem
+        if len(periods) > 2 or not periods :
+            print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+            return
+        
+        #if not is_sequence(periods[0]):
+        if len(periods) == 2:
+            for period in periods:
+                #If either item is not a datetime object, there is a problem
+                if not isinstance(period, datetime.datetime):
+                    print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+                    return
+                
+            start = int(time.mktime(periods[0].timetuple()))
+            end = int(time.mktime(periods[1].timetuple()))
+            
+            #Start time cannot be greater than end time
+            if start > end:
+                print("Error History.change_period(): Start time cannot be newer than end time. Please make sure you are providing the parameters in the correct order: (start, end) \n Period not changed")
+                return
+            
+            self.period[0] = start
+            self.period[1] = end
+            return
+        #If two lists are provided, there is a problem        
+        #if is_sequence(periods[1]):
+        #    print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+         #   return
+        
+        #our last option is that the period is a list of datetimes    
+        for period in periods[0]:
+            #If either item is not a datetime object, there is a problem
+            if not isinstance(period, datetime.datetime):
+                print("Error History.change_period(): change_period requires either 1 list of 2 datetime objects, or 2 datetime objects as parameters. Period not changed")
+                return
+        start = int(time.mktime(periods[0][0].timetuple()))
+        end = int(time.mktime(periods[0][1].timetuple()))
+        if start > end:
+            print("Error History.change_period(): Start time cannot be newer than end time. Please make sure datetimes in list are in the correct order: [start, end] \n Period not changed")
+            return
+        
+        self.period[0] = start
+        self.period[1] = end
+
+    def change_startTime(self, start):
+        if not isinstance(start, datetime.datetime):
+            print("Error History.change_startTime(): change_startTime requires a single datetime object as parameters. Start not changed")
+            return
+        start = int(time.mktime(start.timetuple()))
+        self.period[0] = start
+        
+    def change_endTime(self, end):
+        if not isinstance(end, datetime.datetime):
+            print("Error History.change_endTime(): change_endTime requires a single datetime object as parameters. End not changed")
+            return
+        end = int(time.mktime(end.timetuple()))
+        self.period[1] = end
     
     def get_dataset(self):
         return self.hist_data
 
     def update_dataset(self):
+        if self.period[0]>self.period[1]:
+            print("Error History.update_dataset(): Start time cannot be newer than end time. Please update period \n Data not updated")
+            return
+        
         self.hist_data = hist(self.ticker, self.period, self.interval)
         return self.hist_data
 
@@ -59,7 +147,7 @@ class History():
             print("Error Changing Frequency: num_between is not of type int")
             return
         
-        self.interval = n +'d' if compare == 'daily' else n + 'wk' if compare == 'weekly' else n + 'mo' if compare == 'monthly' else print("Error Changing Frequency: Not a Valid Frequency")
+        self.interval = n +"d" if compare == 'daily' else n + "wk" if compare == 'weekly' else n + "mo" if compare == 'monthly' else print("Error Changing Frequency: Not a Valid Frequency")
 
     def get_date(self, ticker=None):
         #Check if ticker is provided; if ticker is provided and is a list of tickers, return data for those tickers, otherwise return data for that ticker, or if no ticker is provided return data for all tickers 
